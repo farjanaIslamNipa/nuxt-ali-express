b914ffb6db07319dcb5b53f7a6a4f5a63017e694
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-10-10 13:14:44 +0600

    working on header part

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -15,15 +15,83 @@
                     Buyer Protection
                 </li>
                 <li class="px-3 hover:text-[#ff4646] cursor-pointer">
-                    <Icon name="" size="17" />
+                    <Icon name="ic:sharp-install-mobile" size="17" />
+                    App
+                </li>
+                <li
+                    @mouseenter="isAccountMenu = true"
+                    @mouseleave="isAccountMenu = false"
+                    class="relative flex items-center px-2.5 hover:text-[#ff4646] cursor-pointer h-full"
+                    :class="isAccountMenu ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]' : 'border border-[#fafafa]'"
+                    >
+                    <Icon name="ph:user-thin" size="17" />
+                    Account
+                    <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+                    <div
+                        id="AccountMenu"
+                        v-if="isAccountMenu"
+                        class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[106px] border-x border-b"
+                    >
+                        <div v-if="true">
+                             <div class="text-semibold text-[15px] my-4 px-3">Welcome to AliExpress</div>
+                             <div class="flex items-center gap-1 px-3 mb-3">
+                                <NuxtLink
+                                    to="/auth"
+                                    class="bg-[#ff4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
+                                >
+                                    Login/Register
+                                </NuxtLink>
+                             </div>
+                        </div>
+                        <div class="border-b" />
+                        <ul class="bg-white"> 
+                            <li
+                                @click="navigateTo('/orders')"
+                                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+                            >
+                                My Orders
+                            </li>
+                            <li
+                                v-if="true"
+                                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+                            >
+                                Sign out
+                            </li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
+        </div>
+        <div
+            id="MainHeader"
+            class="flex items-center w-full bg-white"
+        >
+            <div class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto">
+                <NuxtLink to="/" class="min-w-[170px]">
+                    <img src="/AliExpress-logo.png" width="170" alt="">
+                </NuxtLink>
+                <div class="max-w-[700px] w-full md:block hidden">
+                    <div class="relative">
+                        <div class="flex items-center border-2 border-[#ff4646] rounded-md w-full">
+                            <input v-model="searchItem" class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none" placeholder="Kitchen accessories" type="text" />
+                            <Icon v-if="isSearching" name="eos-icons:loading" size="25" class="mr-2" />
+                            <button class="flex items-center h-[100%] p-1.5 px-2 bg-[#ff4646]">
+                                <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import {ref} from 'vue'
 
+let isAccountMenu = ref(false)
+let isSearching = ref(false)
+let searchItem = ref('')
 </script>
 
 <style scoped>
