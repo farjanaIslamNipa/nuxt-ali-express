30dbc798f2c0deaf55746086d7795f1674ae8c92
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-10-12 15:54:52 +0600

    checkout page design done

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,7 +1,56 @@
 <template>
   <MainLayout>
     <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="md:flex gap-4 justify-between mx-auto w-full">
+        <div class="md:w-[65%]">
+          <div class="bg-white rounded-lg p-4">
+            <div class="text-xl font-semibold mb-2">Shipping Address</div>
+            <div v-if="true">
+              <NuxtLink to="/address" class="flex items-center pb-2 text-blue-500 hover:text-red-400">
+                <Icon name="mdi:plus" size="18" class="mr-2" />
+                Update Address
+              </NuxtLink>
 
+              <div class="pt-2 border-t">
+                <div class="underline pb-1">Delivery Address</div>
+                <ul class="text-xs">
+                  <li class="flex items-center gap-2">
+                    <div>Contact Name:</div>
+                    <div class="font-bold">Test</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Address:</div>
+                    <div class="font-bold">Test</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Zip Code:</div>
+                    <div class="font-bold">Test</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>City:</div>
+                    <div class="font-bold">Test</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Country:</div>
+                    <div class="font-bold">Test</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <NuxtLink v-else to="/address" class="flex items-center text-blue-500 hover:text-red-400">
+              <Icon name="mdi:plus" size="18" class="mr-2" />
+                Add New Address
+            </NuxtLink>
+          </div>
+
+          <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+            <div v-for="product in products" :key="product.id">
+              <CheckoutItem :product="product" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </MainLayout>
 </template>
@@ -27,6 +76,29 @@ onMounted(() => {
     total.value += item.price
   })
 })
+
+
+watch(() => total.value, () => {
+  if(total.value > 0){
+    stripeInit()
+  }
+})
+
+const stripeInit = async () => {
+
+}
+
+const pay = async () => {
+
+}
+
+const createOrder = async (stripeId) => {
+
+}
+
+const showError = async (errorMsgText) => {
+
+}
 </script>
 
 <style scoped>
